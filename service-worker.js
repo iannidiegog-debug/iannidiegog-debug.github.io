8a7236efd525f3eeb68b1fc64570b73de8261909
iannidiegog-debug <iannidiegog@gmail.com>
2026-05-10 12:43:11 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pedidos-tidial-v14";
+const CACHE_NAME = "pedidos-tidial-v15";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
